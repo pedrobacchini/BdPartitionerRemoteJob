10b254712b7cc5da1d89966e9ef614f5c382296f
pedrobacchini <pedrobacchini@outlook.com>
2022-09-03 16:45:56 -0300

    configure remote partition

--- src/main/java/com/springbatch/bdremotepartitioner/BdPartitionerRemoteJobApplication.java
+++ src/main/java/com/springbatch/bdremotepartitioner/BdPartitionerRemoteJobApplication.java
@@ -2,14 +2,12 @@ package com.springbatch.bdremotepartitioner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BdPartitionerRemoteJobApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(BdPartitionerRemoteJobApplication.class, args);
-		context.close();
+		SpringApplication.run(BdPartitionerRemoteJobApplication.class, args);
 	}
 
 }
--- src/main/java/com/springbatch/bdremotepartitioner/job/BdPartitionerLocalJobConfig.java
+++ src/main/java/com/springbatch/bdremotepartitioner/job/BdPartitionerLocalJobConfig.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("localPartitioner")
 @EnableBatchProcessing
 @Configuration
 public class BdPartitionerLocalJobConfig {
