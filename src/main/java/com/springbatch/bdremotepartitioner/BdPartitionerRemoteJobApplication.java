a97facedcec9b08215d185b46870e09473127cf2
pedrobacchini <pedrobacchini@outlook.com>
2022-09-03 15:09:02 -0300

    configure docker compose basic just for partition local

--- src/main/java/com/springbatch/bdremotepartitioner/BdPartitionerRemoteJobApplication.java
+++ src/main/java/com/springbatch/bdremotepartitioner/BdPartitionerRemoteJobApplication.java
@@ -2,12 +2,14 @@ package com.springbatch.bdremotepartitioner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BdPartitionerRemoteJobApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BdPartitionerRemoteJobApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BdPartitionerRemoteJobApplication.class, args);
+		context.close();
 	}
 
 }
